32dec2e36625a06164a5ed4988e6cff842900e27
Victor Aprea <victor.aprea@wickeddevice.com>
2017-07-26 13:52:37 -0400

    added plot png creation option

--- run.js
+++ run.js
@@ -5,7 +5,12 @@ const argv = require('minimist')(process.argv.slice(2));
 const input = argv.input || "input";
 const dropPercentAfter = (argv.percentAfter || 45) / 100;
 const dropPercentBefore = (argv.percentBefore || 25) / 100;
+const plot = !!argv.plot;
 const fs = require('fs');
+let ChartjsNode = null;
+if(plot){
+  ChartjsNode = require('chartjs-node');
+}
 
 let drop = argv.drop;
 if(drop){
@@ -34,6 +39,66 @@ function runCommand(cmd, options){
 // .catch((error) => {
 //   console.log(error.message, error.stack);
 // });
+
+function generateScatterChart(data, type){
+  if(!plot) return;
+
+  // console.log(JSON.stringify(data, null, 2));
+  // console.log("Type: ", type);
+
+  let plugins = {
+    beforeDraw: function (chartInstance, easing) {
+        let self = chartInstance.config;    /* Configuration object containing type, data, options */
+        let ctx = chartInstance.chart.ctx;  /* Canvas context used to draw with */
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, chartInstance.chart.width, chartInstance.chart.height);
+    }
+  };
+
+  // 600x600 canvas size
+  let chartNode = new ChartjsNode(600, 600);
+  return chartNode.drawChart({
+      type: 'scatter',
+      data: {
+          datasets: [{
+              label: `${type.toUpperCase()} BLV vs Temperature`,
+              data
+          }]
+      },
+      options: {
+          scales: {
+              xAxes: [{
+                  type: 'linear',
+                  position: 'bottom'
+              }]
+          },
+          plugins
+      }
+  })
+  .then(() => {
+      // chart is created
+
+      // get image as png buffer
+      return chartNode.getImageBuffer('image/png');
+  })
+  .then((buffer) => {
+      Array.isArray(buffer) // => true
+      // as a stream
+      return chartNode.getImageStream('image/png');
+  })
+  .then((streamResult) => {
+      // using the length property you can do things like
+      // directly upload the image to s3 by using the
+      // stream and length properties
+      streamResult.stream // => Stream object
+      streamResult.length // => Integer length of stream
+      // write to a file
+      return chartNode.writeImageToFile('image/png', `./${input}.${type}.scatter.png`);
+  })
+  .then(() => {
+    return chartNode.destroy();
+  });
+}
 
 console.log("Running csvdatasmoother with 10M data smoothing...");
 runCommand(`node csvdatasmoother.js --ignoreColumns=7,8,9 --augmented ${drop} --input="${input}.csv" --output="${input}.smoothed.csv" --nostatus --functions="mean,stdev" --duration="PT10M"`)
@@ -107,7 +172,33 @@ runCommand(`node csvdatasmoother.js --ignoreColumns=7,8,9 --augmented ${drop} --
   return runCommand(`node clustertoblv.js --input ${input}.smoothed-culled.csv`);
 })
 .then((result) => {
-  console.log(JSON.stringify(result.stdout.split("\n").filter(l => l.trim()), null, 2));
+  let lines = result.stdout.split("\n").filter(l => l.trim());
+  let data = "["+(/Stats:  \[(.*)\]/g).exec(lines.join(""))[1]+"]";
+  data = JSON.parse(data);
+  console.log(JSON.stringify(data, null, 2));
+  let otherLines = (/Stats:[\s\S]*\[[\s\S]*\]([\s\S]*)/g).exec(lines.join("\n"))[1].split('\n').filter(l => l.trim());
+  otherLines.forEach((l) => {
+    console.log(l);
+  })
+
+  let first_type = otherLines[0].split("_")[0];
+  let second_type = otherLines.slice(-1)[0].split("_")[0];
+
+  return generateScatterChart(data.map((r) => {
+    return {
+      x: +r.temperature_mean,
+      y: +r.voltage1_mean
+    };
+  }), first_type)
+  .then(() => {
+    return generateScatterChart(data.map((r) => {
+      return {
+        x: +r.temperature_mean,
+        y: +r.voltage2_mean
+      };
+    }), second_type);
+  });
+
 })
 .catch((error) => {
   console.log(error.message, error.stack);
